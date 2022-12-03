b8186618d6d3f93a306e1ff4343e93b6ac5c8963
thompson <alfredthompsonovie@gmail.com>
2022-12-03 13:55:04 +0100

    installed font awesome

--- src/main.js
+++ src/main.js
@@ -4,8 +4,21 @@ import router from './router'
 
 import './assets/main.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faSun, faMoon)
+
 const app = createApp(App)
 
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <main>
     <h1>Home page</h1>
+    <font-awesome-icon icon="fa-regular fa-sun" />
+    <font-awesome-icon icon="fa-regular fa-moon" />
   </main>
 </template>
 
